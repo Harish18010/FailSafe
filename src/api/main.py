c63074791a06e75d026c13345b6ebaf793c2a4d4
Harish Kumar N P <nphk1609@gmail.com>
2026-06-07 10:50:48 +0530

    Allow all CORS origins

--- src/api/main.py
+++ src/api/main.py
@@ -22,11 +22,7 @@ app = FastAPI(title="FAILSAFE API", version="1.0")
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=[
-    "http://localhost:5173",
-    "http://127.0.0.1:5173",
-    "https://fail-safe-mocha.vercel.app"
-],
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
